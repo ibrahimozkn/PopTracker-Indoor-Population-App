0dbef1d6f1e72c46a30c4bbc8fc7b30ae2719ff9
JebbyJeb <86562182+JebbyJeb@users.noreply.github.com>
2023-01-03 21:40:50 +0200

    added api routes for auth, business creation, deletion, read, getPopulation

--- api/poptracker1/routes/api.php
+++ api/poptracker1/routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
-use App\Http\Controllers\PopController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PopController;
+use App\Http\Controllers\BusinessController;
+use App\Http\Controllers\PopulationHistoryController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,10 +19,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/business/{id}', [PopController::class, 'addBusiness']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/business', [BusinessController::class, 'getBusinesses']);
+Route::post('/business', [BusinessController::class, 'addBusiness']);
+Route::post('/deleteBusiness/{id}', [BusinessController::class, 'deleteBusiness']);
 Route::get('/population/{id}', [PopController::class, 'getCurrentPopulation']);
+Route::get('/history/{id}', [PopulationHistoryController::class, 'getPopulationHistory']);
 Route::post('/population/add/{id}', [PopController::class, 'IncrementPopulation']);
 Route::post('/population/remove/{id}', [PopController::class, 'DecrementPopulation']);
+
+//Protected Routes (need token to access)
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
