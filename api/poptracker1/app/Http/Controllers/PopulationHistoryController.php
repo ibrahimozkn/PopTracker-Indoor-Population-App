4c753959af8b83aaac1013c65b8e67dce1bd4875
JebbyJeb <86562182+JebbyJeb@users.noreply.github.com>
2023-01-03 21:43:41 +0200

    added getPopulationHistory

--- api/poptracker1/app/Http/Controllers/PopulationHistoryController.php
+++ api/poptracker1/app/Http/Controllers/PopulationHistoryController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\PopulationHistory;
 
 class PopulationHistoryController extends Controller
 {
-    //
+    public function getPopulationHistory($id){
+        $history = PopulationHistory::where('business_id', $id)->get();
+        return $history;
+    }
 }
